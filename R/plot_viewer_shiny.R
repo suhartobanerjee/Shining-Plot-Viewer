38320c71bdef677d628b2a3ef55118b66e496ed5
Suharto Banerjee <suhartobanerjee@gmail.com>
2023-08-30 09:54:09 +0200

    next previous buttons added func
    The buttons get disabled before the edge values. They get re enabled by
    the other button. Using a reactiveVal as a counter, so the plot is
    rendered every time the value is changed. This value is changed within
    the event handlers for the buttons. Beautiful!

--- R/plot_viewer_shiny.R
+++ R/plot_viewer_shiny.R
@@ -1,61 +1,76 @@
 library(shiny)
-source("./utils.R")
+library(shinyjs)
+source("../../../PhD_SandersLab/local_analysis/Digital-Karyotype/R/utils.R",
+    chdir = T
+)
+################################################################################
 
 
+
+################################################################################
+# Main App
 ui <- fluidPage(
-
+    useShinyjs(),
     verticalLayout(
-                   titlePanel("Plot Viewer"),
+        titlePanel("Plot Viewer"),
         wellPanel(
-                  actionButton("prev_button", "Previous"),
-                  actionButton("next_button", "Next")
-                    ),
-            plotOutput("shiny_plot")
+            actionButton("prev_button", "Previous"),
+            actionButton("next_button", "Next")
+        ),
+        plotOutput("shiny_plot")
     )
 )
 
 str(Plot_Digital_Karyotype)
 server <- function(input, output) {
+    counter <- reactiveVal(1)
+    shinyjs::disable("prev_button")
 
-    i <- reactiveVal(1)
+
     observeEvent(input$next_button, {
-            i(i() + 1)
-            output$shiny_plot <- renderPlot({
-                plot(plot_list[[i()]])
-            },
-            width = 1100,
-            height = 850
-            )
+        if (counter() == length(plot_list)) {
+            shinyjs::disable("next_button")
+        } else if (counter() == (length(plot_list) - 1)) {
+            counter(counter() + 1)
+            shinyjs::disable("next_button")
+        } else {
+            shinyjs::enable("prev_button")
+            counter(counter() + 1)
         }
-        )
+    })
+
 
     observeEvent(input$prev_button, {
-            i(i() - 1)
-            output$shiny_plot <- renderPlot({
-                plot(plot_list[[i()]])
-            },
-            width = 1100,
-            height = 850
-            )
-       })
+        if (counter() == 1) {
+            shinyjs::disable("prev_button")
+        } else if (counter() == 2) {
+            counter(counter() - 1)
+            shinyjs::disable("prev_button")
+        } else {
+            shinyjs::enable("next_button")
+            counter(counter() - 1)
+        }
+    })
 
 
-    output$shiny_plot <- renderPlot({
-        plot(plot_list[[i()]])
-    },
-    width = 1100,
-    height = 850
+    output$shiny_plot <- renderPlot(
+        {
+            plot(plot_list[[counter()]])
+        },
+        width = 1100,
+        height = 850
     )
-
 }
 
 
 
-options(shiny.host = '0.0.0.0',
-        shiny.port = 2700
+options(
+    shiny.host = "0.0.0.0",
+    shiny.port = 2700
 )
-shinyApp(ui = ui,
-         server = server
+shinyApp(
+    ui = ui,
+    server = server
 )
 
 
@@ -63,18 +78,22 @@ shinyApp(ui = ui,
 
 # Tests
 plot_list <- list()
-t=seq(0,10,0.1)
-y=sin(t)
-sine_df <- data.frame(t = t,
-                      y = y
+t <- seq(0, 10, 0.1)
+y <- sin(t)
+sine_df <- data.frame(
+    t = t,
+    y = y
 )
-plot_list[[1]] <- ggplot(data = sine_df,
-                         aes(x = t,
-                             y = y
-                         )
+plot_list[[1]] <- ggplot(
+    data = sine_df,
+    aes(
+        x = t,
+        y = y
+    )
 ) +
-                      geom_line()
+    geom_line()
 plot_list[[2]] <- Plot_Digital_Karyotype(plot_ideo_only = T)
-plot_list[[3]] <- Plot_Digital_Karyotype(plot_ideo_only = T,
-                                         plot_both_haplotypes = F
+plot_list[[3]] <- Plot_Digital_Karyotype(
+    plot_ideo_only = T,
+    plot_both_haplotypes = F
 )
