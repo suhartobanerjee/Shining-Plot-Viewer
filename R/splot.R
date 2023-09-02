0f1b9f57830ab6be6614dc36b13218b415147c00
Suharto Banerjee <suhartobanerjee@gmail.com>
2023-09-02 17:31:44 +0200

    starts the server with a blank plot
    with post requests handling, will sub the 1st index and handle the rest
    normally.

--- R/splot.R
+++ R/splot.R
@@ -1,9 +1,13 @@
+library(ggplot2)
 source("./app.R")
 
-sPlot <- function(plot, run_app = F) {
+sPlot <- function(plot = NULL, run_app = F) {
     
+    shinyOptions(plot_list = plot)
 
     if (run_app) {
+        plot <- ggplot()
+
         shinyOptions(plot_list = plot)
         runApp(".")
     }
